ab7abc472e5cbb471d5241b41cd182e2c5e8a2ec
wmhello <871228582@qq.com>
2020-02-04 15:57:58 +0800

    调整因为按需加载引起的补丁

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button type="danger">危险</a-button>
   </div>
 </template>
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+
+            // 给 less-loader 传递 Less.js 相关选项
+            less: {
+                // http://lesscss.org/usage/#less-options-strict-units `Global Variables`
+                // `primary` is global variables fields name
+                globalVars: {
+                    primary: '#fff'
+                },
+                javascriptEnabled: true
+            }
+        }
+    }
+}
